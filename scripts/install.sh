8e9c79121adba3293f61a8ea20fe76a081bfb274
root <stjbrown@gmail.com>
2017-12-15 18:19:43 +0000

    Updated install script & pool playbook

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,5 +7,11 @@ pip install --upgrade pip
 pip install git+git://github.com/ansible/ansible.git@stable-2.4
 pip install ansible[azure] bigsuds f5-sdk netaddr deepdiff ansible-lint ansible-review
 
+#Add dev module bigip_node.py
+cd ~
+git clone https://github.com/F5Networks/f5-ansible -b devel
+mv /usr/lib/python2.7/site-packages/ansible/modules/network/f5/bigip_node.py bigip_node.py.old
+cp ~/f5-ansible/library/bigip_node.py /usr/lib/python2.7/site-packages/ansible/modules/network/f5/bigip_node.py
+
 #Add my git email for dev
 git config --global user.email "stjbrown@gmail.com"
